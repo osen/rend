ea65f7d3b8d10491bda5b9a819bbc95b7385e8ea
Karsten Pedersen <kpedersen@esprimo.local>
2019-12-23 14:07:28 +0000

    Clean up architecture.

--- src/tri/main.c
+++ src/tri/main.c
@@ -1,19 +1,42 @@
 #include <re/re.h>
 #include <SDL2/SDL.h>
 
+struct State;
+
+struct Camera
+{
+  ReVec3 position;
+  ReVec3 rotation;
+  ref(State) state;
+};
+
+struct State
+{
+  ref(Camera) camera;
+  vector(int) keys;
+  ReVec2 mouse;
+  vector(int) buttons;
+};
+
+ref(State) StateCreate();
+void CameraUpdate(ref(Camera) ctx);
+ReMat4 CameraView(ref(Camera) ctx);
+ReMat4 CameraProjection(ref(Camera) ctx);
+
 int main()
 {
   SDL_Window* window = NULL;
   SDL_GLContext glContext = 0;
   ReVec4 col = ReVec4Rgba(1, 1, 1, 1);
   ReMat4 model = ReMat4Identity();
-  ReMat4 view = ReMat4Identity();
-  ReMat4 projection = ReMat4Perspective(45, 1, 0.1f, 100.0f);
   float rotation = 0;
+  ref(State) state = NULL;
 
   ref(ReContext) context = NULL;
   ref(ReRenderer) renderer = NULL;
   ref(ReBuffer) positions = NULL;
+
+  state = StateCreate();
 
   window = SDL_CreateWindow("Re Test",
     SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
@@ -53,6 +76,8 @@ int main()
       }
     }
 
+    CameraUpdate(_(state).camera);
+
     col.x += 0.01f;
     rotation += 1;
 
@@ -67,17 +92,12 @@ int main()
     model = ReMat4Translate(model, ReVec3Xyz(0, 0, -3));
     model = ReMat4RotateZ(model, -rotation * 2);
 
-    view = ReMat4Identity();
-    view = ReMat4Translate(view, ReVec3Xyz(0, 10, 10));
-    view = ReMat4RotateX(view, -45);
-    view = ReMat4Inverse(view);
-
     ReRendererClear(renderer, ReVec4Rgba(0.39f, 0.58f, 0.93f, 1.0f));
 
     ReRendererSetBackfaceCull(renderer, 0);
     ReRendererSetModel(renderer, model);
-    ReRendererSetView(renderer, view);
-    ReRendererSetProjection(renderer, projection);
+    ReRendererSetView(renderer, CameraView(_(state).camera));
+    ReRendererSetProjection(renderer, CameraProjection(_(state).camera));
     ReRendererSetColor(renderer, col);
     ReRendererSetPositionBuffer(renderer, positions);
     ReRendererRender(renderer);
@@ -96,3 +116,38 @@ int main()
   return 0;
 }
 
+ref(State) StateCreate()
+{
+  ref(State) rtn = NULL;
+  ref(Camera) cam = NULL;
+
+  rtn = allocate(State);
+  _(rtn).camera = allocate(Camera);
+
+  cam = _(rtn).camera;
+  _(cam).state = rtn;
+
+  return rtn;
+}
+
+void CameraUpdate(ref(Camera) ctx)
+{
+  _(ctx).position = ReVec3Xyz(0, 10, 10);
+  _(ctx).rotation = ReVec3Xyz(-45, 0, 0);
+}
+
+ReMat4 CameraView(ref(Camera) ctx)
+{
+  ReMat4 rtn = ReMat4Identity();
+
+  rtn = ReMat4Translate(rtn, _(ctx).position);
+  rtn = ReMat4RotateX(rtn, _(ctx).rotation.x);
+  rtn = ReMat4Inverse(rtn);
+
+  return rtn;
+}
+
+ReMat4 CameraProjection(ref(Camera) ctx)
+{
+  return ReMat4Perspective(45, 1, 0.1f, 100.0f);
+}
